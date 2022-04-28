d4a0f6a1fb90ca84377b8614140a589b038973c1
Alexander_Kolyadin <psixsikse666@gmail.com>
2022-04-28 21:55:19 +0300

    add argument Parse

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,4 +1,6 @@
-﻿int num = 5;
+﻿//чтобы перевести один тип данных в другой используется parse
+
+int num = int.Parse(Console.ReadLine());
 int result = num * num;
 //если нужно возвести число в степень int result = Math.Pow(num, 5);
 Console.WriteLine(result);
