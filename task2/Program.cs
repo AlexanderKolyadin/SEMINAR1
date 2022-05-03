d99249270caef2b9bc763a486ccc974dd3028d04
Alexander_Kolyadin <psixsikse666@gmail.com>
2022-05-03 13:07:21 +0300

    add task2

---
+++ task2/Program.cs
@@ -0,0 +1,14 @@
+﻿int num1 = int.Parse(Console.ReadLine());
+int num2 = int.Parse(Console.ReadLine());
+int num1sqare = num1 * num1;
+if (num1sqare == num2) 
+{
+    Console.WriteLine("Да является");
+}
+else
+{
+    Console.WriteLine("Нет.не является");
+}
+
+//bool оператор правда или ложь
+//Console.Writeline(num1sqare ? "да" : "нет");
